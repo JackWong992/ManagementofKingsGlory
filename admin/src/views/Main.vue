027ea83031cab9df56e14a3e00e8a40097c89ee9
JackWong992 <JackWong992@163.com>
2019-06-03 17:01:56 +0800

    updated 2-2

---
+++ admin/src/views/Main.vue
@@ -0,0 +1,75 @@
+<template>
+
+  <el-container class="container">
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+      <el-menu router
+               default-openeds="[1]"
+               :default-active="$route.path">
+        <el-submenu index="1">
+          <template slot="title"><i class="el-icon-message"></i>内容管理</template>
+          <el-menu-item-group>
+            <template slot="title">分组</template>
+            <el-menu-item index="/categories/create">
+              <i class="ifeel">&#xe64b;</i>
+              新建分类
+            </el-menu-item>
+            <el-menu-item index="/categories/list">
+              <i class="ifeel">&#xe63c;</i>
+              分类列表
+            </el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+
+    <el-container>
+      <el-header style="text-align: right; font-size: 12px">
+        <el-dropdown>
+          <i class="el-icon-setting" style="margin-right: 15px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>查看</el-dropdown-item>
+            <el-dropdown-item>新增</el-dropdown-item>
+            <el-dropdown-item>删除</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <span>王小虎</span>
+      </el-header>
+
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+
+<style>
+  .container {
+    height: 100vh;
+  }
+
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    color: #333;
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      const item = {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      };
+      return {
+        tableData: Array(20).fill(item)
+      }
+    }
+  };
+</script>
